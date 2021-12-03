9d0a12a91d63877a3a1ef7c132f61f65e7e1c66e
anshorihsan <anshorihsan@gmail.com>
2021-12-03 15:09:32 +0700

    add hash, decript password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
+import PasswordHash from "../util/PasswordHash";
 const db = require("../db/models");
 
 class AuthController {
   register = async (req: Request, res: Response): Promise<Response> => {
     let {username, password} = req.body;
+    const hashedPassword: string = await PasswordHash.hash(password);
+
     const createdUser = await db.user.create({
       username,
-      password
+      password: hashedPassword
     });
     return res.status(200).send("data berhasil disimpan");
   };
